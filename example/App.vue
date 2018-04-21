930e36d728a36cbe7ed9a7e4dcf44a5617f861c0
lazzzis <shenlijin@outlook.com>
2018-04-20 23:41:29 -0400

    docs: update docs

--- example/App.vue
+++ example/App.vue
@@ -1,42 +1,173 @@
 <template>
-  <div>
+  <div class="markdown-body">
+    <GitHubBadge slug="lazzzis/vue-time-flows" />
+    <h1>Vue Time Flows</h1>
+    <h2>Introduction</h2>
+    <code>vue-time-flows</code> is a zero-dependency vue component for displaying date time. It supports auto-update and format-customize.
+    <h2>Quick Start</h2>
+    <pre><code>npm i -S vue-time-flows // or: yarn add vue-time-flows</code></pre>
+    <h2>Basic Usage</h2>
     <VueTimeFlows />
-    <br />
-    <VueTimeFlows :now="now" />
-    <br />
-    <VueTimeFlows :format="format" :period="2000" />
-    <br />
-    <Current />
-    <br />
-    <Before />
-    <br />
-    <Formatter />
+    <Highlight :code="codes['BasicUsage']" />
+    <Highlight :code="codes['BasicUsageImport']" />
+
+    <h2>Define Start Time</h2>
+    <b>starts on 2017-01-01 (UTC-Time)</b> <VueTimeFlows :now="now" />
+    <Highlight :code="codes['BasicDefineInitialTime']" />
+
+    <h2>Define Format Function</h2>
+    <VueTimeFlows :format="format" />
+    <p><VueTimeFlows :format="httpFormat" /> (<b>HTTP Format</b>) </p>
+    <Highlight :code="codes['BasicDefineFormatFunction']" />
+
+    <h2>Define Update Interval</h2>
+    <b>Update on every 2 seconds</b> <VueTimeFlows :period="2000" />
+    <Highlight :code="codes['BasicDefineFormatFunction']" />
+
+    <h2>Set Default Values</h2>
+    <Highlight :code="codes['SetDefaultValuesImport']" />
+    <Highlight :code="codes['SetDefaultValues']" />
+    <p>Without further configureation, components auto update on 2 second in default and set time to http format</p>
+    <!-- Here is an exception, but I confirm that the code on demo page can work -->
+    <VueTimeFlows :period="2000" :format="httpFormat"/>
+
+    <h2>Create Instances</h2>
+    <p><code>createInstance</code> could help you create various components with different default Values</p>
+    <Highlight :code="codes['CreateInstance']" />
+    <Update2Seconds /> <br>
+    <ISOFormat /> <br>
+    <ISOFormat :now="new Date('2020-01-01').getTime()" />
   </div>
 </template>
 
 <script>
   import { createInstance } from '../src/'
   import { DateTime } from 'luxon'
+  import 'github-markdown-css'
+  import hljs from 'highlight.js'
+  import Highlight from 'vue-highlight-component'
+  import 'highlight.js/styles/atom-one-light.css'
+  import GitHubBadge from 'vue-github-badge'
 
-  function format (val) {
-    return DateTime.fromMillis(val).toFormat('yyyy-LLL-dd HH:mm:ss')
+  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+
+  const codes = {
+    BasicUsage: `<VueTimeFlows />`,
+    BasicUsageImport: `
+    import Vue from 'vue'
+    import VueTimeFlows from 'vue-time-flows'
+
+    Vue.use(VueTimeFlows)`,
+    BasicDefineInitialTime: `
+    <template>
+      <VueTimeFlows :now="now" />
+    </template>
+    <script>
+      export default {
+        data () {
+          return {
+            now: new Date('2017-01-01').getTime()
+          }
+        }
+      }
+    <\/script>`,
+    BasicDefineFormatFunction: `
+    <template>
+      <VueTimeFlows :format="format" /> <br>
+      <VueTimeFlows :format="httpFormat" />
+    </template>
+    <script>
+      import { DateTime } from 'luxon'
+      export default {
+        data () {
+          return {
+            format: (val) => DateTime.fromMillis(val).toFormat('yyyy LLL dd hh:mm:ss'),
+            httpFormat: (val) => DateTime.fromMillis(val).toHTTP()
+          }
+        }
+      }
+    <\/script>
+    `,
+    BasicDefineUpdateInterval: `<VueTimeFlows :period="2000" />`,
+    SetDefaultValuesImport: `
+    import Vue from 'vue'
+    import VueTimeFlows from 'vue-time-flows'
+    import { DateTime } from 'luxon'
+
+    Vue.use(VueTimeFlows, {
+      period: 2000,
+      format: (val) => DateTime.fromMillis(val).toHTTP()
+    })
+    `,
+    SetDefaultValues: `<VueTimeFlows />`,
+    CreateInstance: `
+    <template>
+      <div>
+        <Update2Seconds /> <br>
+        <ISOFormat /> <br>
+        <ISOFormat :now="new Date('2020-01-01').getTime()" />
+      </div>
+    </template>
+    <script>
+      import { createInstance } from 'vue-time-flows'
+      import { DateTime } from 'luxon'
+      const Update2Seconds = createInstance({
+        period: 2000
+      })
+      const ISOFormat = createInstance({
+        format: (val) => DateTime.fromMillis(val).toISO()
+      })
+      export default {
+        components: {
+          Update2Seconds,
+          ISOFormat
+        }
+      }
+    <\/script>
+    `
   }
+
+  const Update2Seconds = createInstance({
+    period: 2000
+  })
+  const ISOFormat = createInstance({
+    format: (val) => DateTime.fromMillis(val).toISO()
+  })
 
   export default {
     components: {
-      Current: createInstance(),
-      Before: createInstance({
-        now: new Date('2017-01-01').getTime()
-      }),
-      Formatter: createInstance({
-        format: (val) => DateTime.fromMillis(val).toFormat('yyyy LLL dd hh:mm:ss')
-      })
+      Update2Seconds,
+      ISOFormat,
+      Highlight,
+      GitHubBadge
     },
     data () {
       return {
-        now: new Date('2000-04-18').getTime(),
-        format
+        now: new Date('2017-01-01').getTime(),
+        format: (val) => DateTime.fromMillis(val).toFormat('yyyy LLL dd hh:mm:ss'),
+        httpFormat: (val) => DateTime.fromMillis(val).toHTTP(),
+        codes
       }
     }
   }
 </script>
+
+<style>
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
+
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
+  .vue-time-flows {
+    font-size: 1.5em;
+  }
+</style>
--- poi.config.js
+++ poi.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   entry: 'example',
-  dist: 'example/dist',
+  outDir: 'example/dist/',
+  homepage: '/vue-time-flows/',
   babel: {
     jsx: 'vue'
   }
